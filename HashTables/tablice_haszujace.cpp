c2be1c08d6ddb4eecf13c1a226b93db738aa32fd
janek_g <jmgrzesik@gmail.com>
2015-05-09 10:39:21 +0200

    Minor corrections in HashTables. Sill having some problems with template types

--- HashTables/tablice_haszujace.cpp
+++ HashTables/tablice_haszujace.cpp
@@ -1,22 +1,25 @@
 //
 // Created by Janusz Grzesik on 08.05.15.
 //
-
+#include "iostream"
 #include "tablice_haszujace.h"
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     HashTable<int>* ht = new HashTable<int>(11);
-    int o = 5;
-    int t = 8;
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    insertion_double(*ht,2,hash_function_one, hash_function_two);
-    ht->print_hash_table();
+//    int o = 5;
+//    int t = 8;
 
+//    insertion_double(*ht,2,hash_function_one, hash_function_two);
+
+    insertion_linear(*ht, 2, hash_function_one);
+    insertion_linear(*ht, 2, hash_function_one);
+    insertion_linear(*ht, 7, hash_function_one);
+    insertion_linear(*ht, 3, hash_function_one);
+    remove_linear(*ht, 2, hash_function_one);
+    remove_linear(*ht, 2, hash_function_one);
+    remove_linear(*ht, 3, hash_function_one);
+    remove_linear(*ht, 3, hash_function_one);
+    ht->print_hash_table();
     return 0;
 }
--- HashTables/tablice_haszujace.h
+++ HashTables/tablice_haszujace.h
@@ -41,6 +41,11 @@ public:
     void compute_load_factor(){
         load_factor = number_of_elements *1.0 / size;
     }
+    void delete_if_pointer(TYPE* &t){
+        delete t;
+        t = nullptr;
+    }
+    void delete_if_pointer(TYPE t){};
 
     void print_hash_table(){
         for(int i = 0 ; i<this->size; i++){
@@ -48,7 +53,6 @@ public:
                 std::cout<<"_"<<std::endl;
             }
             else{
-
                 std::cout<< *(this->table[i].data)<<std::endl;
             }
         }
@@ -65,6 +69,19 @@ bool insert_at_index(HashTable<TYPE> hashTable, TYPE new_data, int index){
     }
     hashTable.table[index].data = new TYPE(new_data);
     hashTable.number_of_elements++;
+    hashTable.compute_load_factor();
+    return true;
+}
+template <class TYPE>
+bool remove_at_index(HashTable<TYPE> hashTable,TYPE data_to_remove, int index){
+    //This function removes the node at given index
+    //If tha HashTable at the given index already has an item it will return false
+    if(hashTable.table[index].data== nullptr || *(hashTable.table[index].data) != data_to_remove){
+        return false;
+    }
+    hashTable.delete_if_pointer(hashTable.table[index].data);
+    hashTable.table[index].deleted=true;
+    hashTable.number_of_elements--;
     hashTable.compute_load_factor();
     return true;
 }
@@ -88,6 +105,20 @@ bool insertion_linear(HashTable<TYPE> hashTable, TYPE data, int (*f)(int ,TYPE))
     int index = f(hashTable.size, data);
     for(int i= 0; i< hashTable.size; i++){
         if(insert_at_index(hashTable,data,(index + i)%hashTable.size)){
+            return true;
+        }
+    }
+    return false;
+}
+
+template <class TYPE>
+bool remove_linear(HashTable<TYPE> hashTable, TYPE data_to_remove, int (*f)(int ,TYPE)){
+    if(hashTable.load_factor==1.0){
+        return false;
+    }
+    int index = f(hashTable.size, data_to_remove);
+    for(int i= 0; i< hashTable.size; i++){
+        if(remove_at_index(hashTable,data_to_remove,(index + i)%hashTable.size)){
             return true;
         }
     }
